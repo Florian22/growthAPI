4b97888b335978d7a1ebe8bbd4e1cecd37e55c69
Florian <florian.bonniec22@gmail.com>
2017-10-14 13:13:48 +0200

    correction wrong controller called in categoryRoutes

--- src/modules/categories/category.routes.js
+++ src/modules/categories/category.routes.js
@@ -8,5 +8,5 @@ const routes = new Router();
 
 routes.post('/', authJwt, categoryController.createCategory);
 routes.get('/:id', categoryController.getCategoryById);
-routes.get('/', plantController.getCategoryList);
+routes.get('/', categoryController.getCategoryList);
 export default routes;
