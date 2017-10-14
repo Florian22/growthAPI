652da54fb45b76c8a555b7c1789bb96eddec8e9b
Florian <florian.bonniec22@gmail.com>
2017-10-14 11:50:36 +0200

    routes debug

--- src/modules/categories/category.routes.js
+++ src/modules/categories/category.routes.js
@@ -1,8 +1,8 @@
 import {Router} from 'express';
 import * as categoryController from './category.controllers';
 import { authJwt } from '../../services/auth.services';
-import validate from 'express-validation';
-import categoryValidation from './category.validation';
+//import validate from 'express-validation';
+//import categoryValidation from './category.validation';
 
 const routes = new Router();
 //routes.post('/', authJwt, validate(categoryValidation.addPlant),plantController.addPlant);
