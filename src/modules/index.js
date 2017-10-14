9bb0c77edecbcbf63652538a623899ef9917ed1e
Florian <florian.bonniec22@gmail.com>
2017-10-14 11:24:49 +0200

    debug routes

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,10 +2,11 @@ import userRoutes from './users/user.routes';
 import plantRoutes from './plants/plant.routes';
 import categoryRoutes from './categories/category.routes';
 import {authJwt} from '../services/auth.services';
+
 export default app => {
   app.use('/api/v1/users', userRoutes);
-  app.use('/api/v1/plants', plantRoutes);
-  app.use('/api/v1/category', categoryRoutes);
+  //app.use('/api/v1/plants', plantRoutes);
+  //app.use('/api/v1/category', categoryRoutes);
   app.get('/hello', authJwt ,(req,res)=>{
   	res.send('This is a private route, I hope so !!!');
   });
